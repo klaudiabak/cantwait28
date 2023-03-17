51e108e503b37cccb847df774d370f77779e9716
Klaudia Bąk <aoimesan@gmail.com>
2023-03-17 12:15:32 +0100

    Add Firebase Web App

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCf_10RuhKfCepezk3Cf-xkk7FZnSkBVZw",
+  authDomain: "cantwait377.firebaseapp.com",
+  projectId: "cantwait377",
+  storageBucket: "cantwait377.appspot.com",
+  messagingSenderId: "50649752002",
+  appId: "1:50649752002:web:8d332152a81105a70fb65b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
